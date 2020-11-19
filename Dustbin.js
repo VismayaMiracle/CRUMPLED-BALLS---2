2d1e002a271d46ab5496c90d2e246e40952d82ef
VismayaMiracle <72506678+VismayaMiracle@users.noreply.github.com>
2020-11-19 14:28:53 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,25 @@
+class Dustbin{
+    constructor(x,y,width,height){
+       var options ={
+          ' isStatic':true,
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+       }
+       this.body = Bodies.rectangle(x,y,width,height,options);
+       this.width = width;
+       this.height = height;
+      
+       this.image = loadImage("Images/dustbingreen.png");
+       
+       World.add(world,this.body);
+      
+    };
+    display(){
+        var pos =this.body.position;
+        imageMode(CENTER);
+        fill(255, 255, 255, 0.5);
+        image(this.image,pos.x, pos.y, this.width, this.height);
+        
+      }
+}
